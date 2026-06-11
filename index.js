39f2644065552f40f2820b0bd100951eb3eba86a
Venkatesh <venkateshv2k10@gmail.com>
2026-06-11 22:28:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const container = document.getElementById("image-container");
 const img = document.createElement("img");
 const img_to_str ={
-    0: "/Images/selector.png",
-    1: "/Images/font-size.png",
-    2: "/Images/main-black.png",
-    3: "/Images/main-white.png",
-    4: "/Images/any-font.png"
+    0: "selector.png",
+    1: "font-size.png",
+    2: "main-black.png",
+    3: "main-white.png",
+    4: "any-font.png"
 };
 let cur_img = 0;
-img.src = "/Images/selector.png"
+img.src = "/chemistry-record/Images/selector.png"
 img.alt = ""
 container.appendChild(img)
 
@@ -16,7 +16,7 @@ function update_image(cur_img){
     container.removeChild(container.lastChild);
     cur_img += 1;
     cur_img %= 5;
-    img.src = img_to_str[cur_img]
+    img.src = `/chemistry-record/Images/${img_to_str[cur_img]}`
     img.alt = ""
     container.appendChild(img)
     return cur_img
